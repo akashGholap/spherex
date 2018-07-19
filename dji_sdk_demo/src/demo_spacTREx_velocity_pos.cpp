f2f2ef092f7b159e58e599d4bd57b3697d87b6c6
Akash Gholap <akashgholapchem.sci@gmail.com>
2018-07-19 03:43:32 -0700

    Minor Errors

--- dji_sdk_demo/src/demo_spacTREx_velocity_pos.cpp
+++ dji_sdk_demo/src/demo_spacTREx_velocity_pos.cpp
@@ -641,7 +641,8 @@ bool set_optimum_velocity()   //not yet prototyped
     {
       double t;
       const double begin = 0.1;
-      const double starting_point = 0.1;
+      double begin1 = 0.1;
+      double& starting_point = begin1;
       const double end = 3;
       const double eps = 0.1;
       const long max_iter = 10;
