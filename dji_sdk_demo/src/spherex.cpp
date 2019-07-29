61a1cc6e274d3ddd28760cef3c9859ee41a5ae05
Akash Gholap <akashgholapchem.sci@gmail.com>
2019-07-28 18:03:42 -0700

    up vel 1.8

--- dji_sdk_demo/src/spherex.cpp
+++ dji_sdk_demo/src/spherex.cpp
@@ -196,7 +196,7 @@ void getVelocity_callback(const geometry_msgs::Vector3Stamped& vel_from_sdk) // 
     }
     else if(hop.wait_counter>100&&hop.up_counter<=10)
     {
-      hop.hop_fill_vel(0,0,2,0);
+      hop.hop_fill_vel(0,0,1.8,0);
       hop.up_counter++;
       hop.finished = false;
     }
